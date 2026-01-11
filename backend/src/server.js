500e263985c2ae3dd914f6967c9fe734cbec601d
Forhad <forhaduddin009@gmail.com>
2026-01-10 23:44:55 -0500

    middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js"
 //const express = require("express");
 
 dotenv.config()
@@ -9,16 +10,16 @@ dotenv.config()
 const app= express();
 const PORT= process.env.PORT || 5001;
 
-connectDB();
+
 app.use(express.json())
+app.use(rateLimiter)
+
 app.use("/api/notes", notesRoutes)
 
+connectDB().then( ()=>{     
+    app.listen(PORT, () =>{
+        console.log("Server started at PORT:", PORT);
 
-app.listen(PORT, () =>{
-    console.log("Server started at PORT:", PORT);
-
+    })
 })
 
-// l4AIExgzkRpiuutX
-
-//mongodb+srv://idkshafin01_db_user:l4AIExgzkRpiuutX@cluster0.fmfp4sg.mongodb.net/?appName=Cluster0
